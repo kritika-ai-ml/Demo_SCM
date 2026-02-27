eb15497cae672c23ecdd348a6c8359d547fcf152
kritika-ai-ml <kritikachaudhary987@gmail.com>
2026-02-28 01:39:30 +0530

    updated cors config

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const complaintRoutes = require('./routes/complaints');
 const app = express();
 
 // ================= MIDDLEWARE =================
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
